1629efa216de167e3cc53baa07796d39f66ba080
Leonardo Morais <leonardomorais89@yahoo.com.br>
2020-10-28 17:27:28 -0300

    Melhoria

--- src/main/java/log/pattern/util/LogUtil.java
+++ src/main/java/log/pattern/util/LogUtil.java
@@ -1,5 +1,8 @@
 package log.pattern.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.util.StopWatch;
 
 import lombok.extern.slf4j.Slf4j;
@@ -7,31 +10,43 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class LogUtil {
 
-	public static int beginMethod(String textLog) {
+	public static List<Object> beginMethod(String textLog) {
+		StopWatch sw = new StopWatch(textLog);
+		sw.start();
 		textLog = String.format("<<<<< %s >>>>>", textLog);
 		int size = textLog.length();
 		log.info("=".repeat(size));
 		log.info(textLog);
 		log.info("=".repeat(size));
-		return size;
+		List<Object> list = new ArrayList<>();
+		list.add((Object) size);
+		list.add((Object) sw);
+		return list;
 	}
 
-	public static int litenService(String textLog) {
+	public static List<Object> litenService(String textLog) {
+		StopWatch sw = new StopWatch(textLog);
+		sw.start();
 		textLog = String.format("<<<<< %s >>>>>", textLog);
 		int size = textLog.length();
 		log.info("-".repeat(size));
 		log.info(textLog);
 		log.info("-".repeat(size));
-		return size;
+		List<Object> list = new ArrayList<>();
+		list.add((Object) size);
+		list.add((Object) sw);
+		return list;
 	}
 
-	public static void logTime(StopWatch sw, int size, boolean isEndMethod) {
+	public static void logTime(List<Object> list, boolean isEndMethod) {
+		int size = (int) list.get(0);
+		StopWatch sw = (StopWatch) list.get(1);
 		sw.stop();
 		log.info(String.format("%s - TEMPO: %s", sw.getId(), sw.getTotalTimeSeconds()));
-		int metade1 = (size - 5) / 2;
-		int metade2 = (size % 2 == 0) ? metade1 : metade1 + 1;
 		if (isEndMethod) {
-			log.info("=".repeat(metade1) + " FIM " + "=".repeat(metade2));
+			int metade1 = (size - 5) / 2;
+			int metade2 = (size % 2 == 0) ? metade1 : metade1 + 1;
+			log.info(String.format("%s%s%s", "=".repeat(metade1), " FIM ", "=".repeat(metade2)));
 		} else {
 			log.info("-".repeat(size));
 		}
